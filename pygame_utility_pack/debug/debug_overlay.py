e529ae3ce1887c7ecb31799b4a9734265a6a9e90
Joshua Abbott Salazar <joshjaysalazar@gmail.com>
2023-05-02 16:53:04 -0500

    Added lots of comments

--- pygame_utility_pack/debug/debug_overlay.py
+++ pygame_utility_pack/debug/debug_overlay.py
@@ -12,15 +12,21 @@ class DebugOverlay:
     def __init__(self, screen):
         """
         Initializes the DebugOverlay with the given Pygame screen.
-
-        :param screen: the Pygame screen to draw on
-        :type screen: pygame.Surface
         """
 
+        # Member variables
         self.screen = screen
+
+        # Default font for the DebugOverlay
         self.font = pygame.font.SysFont('Courier New', 16, bold=True)
+
+        # Dictionary to track variables to display on the overlay
         self.variables = {}
+
+        # Flag indicating whether to draw a background
         self.background_enabled = False
+
+        # Surface used to draw the DebugOverlay, set to the size of the screen
         self.overlay_surface = pygame.Surface(
             pygame.display.get_surface().get_size(),
             pygame.SRCALPHA
@@ -62,10 +68,13 @@ class DebugOverlay:
         :raises TypeError: if name is not a string or if size is not an integer
         """
 
+        # Check for invalid arguments
         if not isinstance(name, str):
             raise TypeError("Font name must be a string")
         if not isinstance(size, int):
             raise TypeError("Font size must be an integer")
+        
+        # Set the font
         self.font = pygame.font.SysFont(name, size, bold, italic)
 
     def enable_background(self):
@@ -87,18 +96,26 @@ class DebugOverlay:
         Draws the DebugOverlay on the Pygame screen.
         """
 
+        # Clear the overlay surface
         self.overlay_surface.fill((0, 0, 0, 0))
 
+        # Track the y offset for each line of text
         y_offset = 0
 
+        # Draw each variable on the overlay surface
         for var_name, value in self.variables.items():
             text = self.font.render(f"{var_name}: {value}", True, "white")
+
+            # Draw a background for the text if enabled
             if self.background_enabled:
                 text_rect = text.get_rect()
                 x, y = text_rect.left, text_rect.top + y_offset
                 w, h = text_rect.width, text_rect.height
                 pygame.draw.rect(self.overlay_surface, "black", (x, y, w, h))
+            
+            # Draw the text on the overlay surface
             self.overlay_surface.blit(text, (0, y_offset))
             y_offset += self.font.get_linesize()
 
+        # Draw the overlay surface on the Pygame screen
         self.screen.blit(self.overlay_surface, (0, 0))
